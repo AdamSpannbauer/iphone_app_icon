66a93e1c3eae1c6e1723389968263348db3e5099
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-02-28 07:53:02 -0500

    added by row option to results montage

--- utils/resultsmontage.py
+++ utils/resultsmontage.py
@@ -3,15 +3,24 @@ import numpy as np
 import cv2
 
 class results_montage:
-	def __init__(self, image_size, images_per_row, num_results):
+	"""
+	class to combine input images into a single image displaying a combined grid 
+	of the input images
+	"""
+	def __init__(self, image_size, images_per_main_axis, num_results, by_row=True):
 		# store the target image size and the number of images per row
 		self.imageW = image_size[0]
 		self.imageH = image_size[1]
-		self.images_per_row = images_per_row
+		self.images_per_main_axis = images_per_main_axis
+		self.by_row = by_row
 
 		# allocate memory for the output image
-		num_cols = -(-num_results // images_per_row) #ceiling division
-		self.montage = np.zeros((num_cols * self.imageW, images_per_row * self.imageH, 3), dtype="uint8")
+		num_main_axis = -(-num_results // images_per_main_axis) #ceiling division
+		if by_row:
+			self.montage = np.zeros((num_main_axis * self.imageW, images_per_main_axis * self.imageH, 3), dtype="uint8")
+		else:
+			self.montage = np.zeros((images_per_main_axis * self.imageW, num_main_axis * self.imageH, 3), dtype="uint8")
+
 
 		# initialize the counter for the current image along with the row and column
 		# number
@@ -20,11 +29,16 @@ class results_montage:
 		self.col = 0
 
 	def add_result(self, image, text=None, highlight=False):
-		# check to see if the number of images per row has been met, and if so, reset
-		# the column counter and increment the row
-		if self.counter != 0 and self.counter % self.images_per_row == 0:
-			self.col = 0
-			self.row += 1
+		# check to see if the number of images per row/col has been met, and if so, reset
+		# the row/col counter and increment the row
+		if self.by_row:
+			if self.counter != 0 and self.counter % self.images_per_main_axis == 0:
+				self.col = 0
+				self.row += 1
+		else:
+			if self.counter != 0 and self.counter % self.images_per_main_axis == 0:
+				self.col += 1
+				self.row = 0
 
 		# resize the image to the fixed width and height and set it in the montage
 		image = cv2.resize(image, (self.imageH, self.imageW))
@@ -41,6 +55,14 @@ class results_montage:
 		if highlight:
 			cv2.rectangle(self.montage, (startX + 3, startY + 3), (endX - 3, endY - 3), (0, 255, 0), 4)
 
-		# increment the column counter and image counter
-		self.col += 1
+		if self.by_row:
+			# increment the column counter
+			self.col += 1
+		else:
+			# increment the row counter
+			self.row += 1
+		# increment total image counter
 		self.counter += 1
+
+
+
