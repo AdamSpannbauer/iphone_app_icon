86bbfc1148023c4f11c7ab877817b06d40869eed
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-03-01 22:08:47 -0500

    added example plot output

--- dominant_color_plot.py
+++ dominant_color_plot.py
@@ -8,15 +8,19 @@ import numpy as np
 #apps to plot
 apps_of_interest = {'Facebook': 'free-apps_facebook.jpg',
 					'WhatsApp': 'free-apps_whatsapp_messenger.jpg',
-					'Instagram': 'free-apps_instagram.jpg',
+					'Waze': 'free-apps_waze_navigation_live_traffic.jpg',
 					'Soundcloud': 'free-apps_soundcloud_music_audio.jpg'}
 
 #function to gen random stats
-def gen_random_walk(start=100, n_steps=50, num_range=(-30,30)):
+def gen_random_walk(start=100, n_steps=50, num_range=(-100,100), new_step_chance=0.5):
 	walk = [start]
+	step_val = random.randrange(num_range[0], num_range[1])
 	for i in range(n_steps):
-		step_val = random.randrange(num_range[0], num_range[1])
+		#chance to take a new step or take last step again
+		if random.random() > (1-new_step_chance):
+			step_val = random.randrange(num_range[0], num_range[1])
 		new_pos = walk[i-1] + step_val
+
 		walk.append(new_pos)
 	return walk
 
@@ -27,13 +31,13 @@ plot_x = range(1,101)
 #iterate over app
 for name, path in apps_of_interest.items():
 	#gen data
-	app_data = gen_random_walk(start=random.randrange(1000,2000))
+	app_data = gen_random_walk(start=random.randrange(1000,2000), new_step_chance=0.3)
 	#read in image
 	bgr_image = cv2.imread('icons/{}'.format(path))
 	#convert to HSV; this is a better representation of how we see color
 	hsv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
 	#get dominant color in app icon
-	hsv = get_dominant_color(hsv_image)
+	hsv = get_dominant_color(hsv_image, k=5)
 	#make into array for color conversion with cv2
 	hsv_im = np.array(hsv, dtype='uint8').reshape(1,1,3)
 	
@@ -55,8 +59,19 @@ for name, path in apps_of_interest.items():
 	#append trace to plot data
 	plot_traces.append(trace)
 
+layout = go.Layout(
+		title = "App Trends in Fake Statistic",
+		xaxis = dict(
+			showticklabels = False,
+			ticks = '',
+			title = 'Time (probably)'
+			)
+		)
+
+fig = go.Figure(data=plot_traces, layout=layout)
+
 #produce plot output
-plot(plot_traces)
+plot(fig, config={'displayModeBar': False})
 
 
 
